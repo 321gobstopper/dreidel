ca873d4a5e07b52390834c8f415dadf91be42f8f
321gobstopper <321gobstopper@gmail.com>
2024-12-27 11:42:06 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        String message = "hello world";
-        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you name: ");
+        String name = scanner.nextLine(); // Read user input
+        if (name != null && name.length() > 0) {
+            name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        }
+        else{ 
+            name = "Person. I can't believe you were to lazy to enter your name";
+        }
+        System.out.println("Hello " + name + "! You are awesome!");
+        scanner.close();
+
     }
 }
